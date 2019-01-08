f2fd33f90b84a8bf66ee6bfd614839bd0fe46661
Narayana Rao <narayanarao.kandregula@gmail.com>
2019-01-08 14:11:13 +0530

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Narayana Rao on 08/01/19.
+//  Copyright © 2019 Narayana Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
